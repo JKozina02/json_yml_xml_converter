0d05ea53fe9eea5e4f595a198b454731b9a48f08
Jędrzej <jedrek.kozina02@gmail.com>
2023-05-26 21:38:32 +0200

    Added conversion of json files to xml and yaml

--- conversion.py
+++ conversion.py
@@ -1,8 +1,8 @@
 import sys
 import json, xmltodict, yaml
 
-file_name = sys.argv[1]
-file_object = open(file_name, "r")
+file = sys.argv[1]
+file_object = open(file, "r")
 #new_file = str(sys.argv[2])
 
 def file_format(file): #returns format of a file
@@ -48,8 +48,36 @@ def is_valid(file, type): #checks if format is correct
         case "wrong":
             return False
 
-format = file_format(file_name)
-print(is_valid(file_object, format))
-print(format)
+def convert_json(file,new_type):
+
+    with open(file) as input:
+        jsonStr = input.read()
+        obj = json.loads(jsonStr)
+
+    match new_type:
+        case "xml":
+            print("konwersja json na xml")
+            try:
+                new_file = open("xml_converted_file.xml", "w")
+                new_file.write(xmltodict.unparse(obj,))
+                new_file.close()
+            except ValueError as error:
+                print("Cant convert that file to xml: ", error)
+        case "yml" | "yaml":
+            try:
+                new_file = open("yaml_converted_file.yml", "w")
+                new_file.write(yaml.dump(obj))
+                new_file.close()
+            except ValueError as error:
+                print("Cant convert that file to yaml: ", error)
+        case "json":
+            print("Plik już jest w formacie json")
+            return file
+
+my_format = file_format(file)
+print(my_format)
+print(is_valid(file_object, my_format))
+convert_json(file, "xml")
+
 
 
